2917b11d990406d510851586c363de2afef4bf73
NFFY88 <baksenok@yandex.ru>
2026-05-20 20:00:04 +0300

    copyright

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -135,7 +135,7 @@ const config: Config = {
       //     ],
       //   },
       // ],
-      // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} <a href="https://t.me/meshtastic37">Meshtastic Ivanovo Community</a>`,
     },
     prism: {
       theme: prismThemes.github,
