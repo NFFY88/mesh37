4931ee8511dc9b39232baf7338597b9ceb8b8589
NFFY88 <baksenok@yandex.ru>
2026-05-27 23:21:10 +0300

    quick start

--- src/components/HomepageWelcome/index.tsx
+++ src/components/HomepageWelcome/index.tsx
@@ -6,7 +6,8 @@ const HomepageWelcome = () => {
     <section className={styles.welcome}>
       <div className="container">
         <Heading as="h2" className="text--center">Добро пожаловать</Heading>
-        <p>Давайте дружно крикнем "Аааааааааа" и, возможно, нас услышат</p>
+        <p>Данный сайт содержит информацию о mesh сетях на базе LoRa в Иванове. Более подробную информацию вы можете найти в разделе <a href="/docs/intro">Документация</a></p>
+        <p>В данный момент сайт находится в стадии разработки, поэтому просим понять и простить</p>
       </div>
     </section>
   );
