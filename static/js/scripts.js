722a4f2d253892454b6589677460f56fa0b6eb32
NFFY88 <baksenok@yandex.ru>
2026-05-29 01:04:08 +0300

    metrika

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,5 +3,12 @@ const { isProduction, isStaging } = window.SITE_CONFIG;
 const PROD_HOSTNAME = 'mesh37.ru';
 
 if (isProduction && window.location.hostname === PROD_HOSTNAME) {
-    console.log('Production mode');
+     (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109481424', 'ym');
+
+    ym(109481424, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
 }
