779c69073ea1d6e3810d5e5bf79e6a28d64c1b83
NFFY88 <baksenok@yandex.ru>
2026-05-29 00:51:43 +0300

    test

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 const { isProduction, isStaging } = window.SITE_CONFIG;
 
-const PROD_HOSTNAME = 'mesh37.com';
+const PROD_HOSTNAME = 'mesh37.ru';
 
 if (isProduction && window.location.hostname === PROD_HOSTNAME) {
     console.log('Production mode');
